2b084ab3d08f433e9b409c6432804bf88801c629
Student25 <student25@HackaTruck25.local>
2023-08-18 15:53:40 -0300

    initial commit

---
+++ DiviDApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DiviDApp
+//
+//  Created by Student25 on 18/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
