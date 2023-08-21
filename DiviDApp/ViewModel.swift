812eb39ea7a4a0a18e089455aee02d4ac4e35f0a
Student25 <student25@HackaTruck25.local>
2023-08-21 14:35:35 -0300

    added base function

--- DiviDApp/ViewModel.swift
+++ DiviDApp/ViewModel.swift
@@ -4,5 +4,29 @@ import Foundation
 
 class ViewModel: ObservableObject {
     
+    @Published var usuarios: [Usuario] = []
+    
+    func fetchUsuarios(){
+        guard let url = URL(string: "https://anapioficeandfire.com/api/houses") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url){ [weak self] data, _, error in
+            guard let data = data, error == nil else{
+                return
+            }
+            
+            do {
+                let parsed = try JSONDecoder().decode([Usuario].self, from: data)
+                
+                DispatchQueue.main.async {
+                    self?.usuarios = parsed
+                }
+            }catch{
+                print(error)
+            }
+        }
+        task.resume()
+    }
     
 }
