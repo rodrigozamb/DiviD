bbf87cd0c2ca7a76cc05307f9aea293203ff88ad
Guilherme0202PM <46054442+Guilherme0202PM@users.noreply.github.com>
2023-08-21 16:05:38 -0300

    initial commit

--- DiviDApp/DetailViewTela4.swift
+++ DiviDApp/DetailViewTela4.swift
@@ -1,75 +1,70 @@
-//
-//  DetailViewTela4.swift
-//  DiviDApp
-//
-//  Created by Student10 on 21/08/23.
-//
-
 import SwiftUI
 
 struct DetailViewTela4: View {
+    let ids = [1, 2, 3 , 4, 5,6,7,8,9] // Seus IDs aqui
+
     var body: some View {
         TabView {
-            ZStack{
+            
+            ZStack {
+                
                 LinearGradient(gradient: Gradient(colors: [.blue]), startPoint: .top, endPoint: .bottom).ignoresSafeArea()
-                                
-                RoundedRectangle(cornerRadius: 25).fill(.white).frame(width: 400, height: 620).offset(x:0,y: 130)
 
-
+                RoundedRectangle(cornerRadius: 25).fill(.white).frame(width: 400, height: 620).offset(x:0,y: 145)
+                
                 VStack {
-                    
-                    HStack{
-                        
+
+                    HStack {
                         Spacer()
-                        
+
                         Text("RACHA")
                             .font(.system(size: 35))
                             .fontWeight(.bold)
                             .padding()
-                        
+
                         Label("", systemImage: "gearshape.fill").font(.system(size: 70))
                     }
-                    
-                    HStack{
+
+                    HStack {
                         Label("5", systemImage: "person.crop.circle.fill").font(.system(size: 35))
-                        
+
                         Label("", systemImage: "cart.badge.questionmark").font(.system(size: 80))
-                        
+
                         Text("15/08").font(.system(size: 20))
                     }
-                                        
-                    RoundedRectangle(cornerRadius: 25).fill(.gray).frame(width: 300, height: 150)
-                    
-                    RoundedRectangle(cornerRadius: 25).fill(.gray).frame(width: 300, height: 150)
-                    
-                    RoundedRectangle(cornerRadius: 25).fill(.gray).frame(width: 300, height: 150)
-                    
-                    
-                    
-                    Text("show")
-                        .multilineTextAlignment(.leading)
-                        .padding(.horizontal).font(.system(size: 35))
-                        
-                        //.overlay(
-                          //  RoundedRectangle(cornerRadius: 16)
-                           //     .stroke(.gray, lineWidth: 40)
-                        //)
-                    
-                    Spacer()
-                    
-                   
-                }
-                
-            }//fim ZStack
+
+                    ScrollView {
+                        VStack{
+                            ForEach(ids, id: \.self) { id in
+                                
+                                VStack{
+                                    Text("Texto do retângulo \(id)")
+                                    
+                                    HStack{
+                                        Text("Algum Valor \(id)")
+                                        Spacer()
+                                        Text("Outro Valor \(id)")
+
+                                    }//Fim HStack
+                                    
+                                }
+                                .frame(width: 300, height: 150).foregroundColor(.black)
+                                .background{Color.gray}.cornerRadius(25)
+                                
+                            } // Fim ForEach
+                        } //Fim VStack ScrollView
+                    } // Fim ScrollView
+
+                    //Spacer()
+                } // fim ZStack
+
+            } // fim ZStack
         }
     }
 }
 
-
 struct DetailViewTela4_Previews: PreviewProvider {
     static var previews: some View {
         DetailViewTela4()
-
-
     }
 }
