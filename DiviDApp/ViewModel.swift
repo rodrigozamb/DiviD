1b6c06fa8f48e8fe4c95877718c7565b32310fcd
Otavio Campos <otavioc889@gmail.com>
2023-08-21 15:32:01 -0300

    commit 15:31

--- DiviDApp/ViewModel.swift
+++ DiviDApp/ViewModel.swift
@@ -7,7 +7,7 @@ class ViewModel: ObservableObject {
     @Published var usuarios: [Usuario] = []
     
     func fetchUsuarios(){
-        guard let url = URL(string: "http://192.168.128.143:1880/dividusuarios") else {
+        guard let url = URL(string: "http://192.168.128.143:1880/dividregistrar") else {
             return
         }
         
