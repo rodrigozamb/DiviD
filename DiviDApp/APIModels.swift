bb99ebb425ae9741847df73a11e946bf6cd57fbd
Rodrigo Zamboni <rodrigo-zamboni@hotmail.com>
2023-08-22 16:59:25 -0300

    added id field on app structs

--- DiviDApp/APIModels.swift
+++ DiviDApp/APIModels.swift
@@ -7,30 +7,34 @@
 
 import Foundation
 
-struct usuario: Codable, Hashable{
+struct usuario: Codable, Hashable, Identifiable{
+    let id: String
     let nome: String
     let email: String
     let senha: String
     let idade: Int
 }
 
-struct divida: Codable, Hashable{
+struct divida: Codable, Hashable, Identifiable{
+    let id: String
     let userID: String
     let despesaID: String
     let valor: Double
     let status: Bool
 }
 
-struct despesa: Codable, Hashable{
+struct despesa: Codable, Hashable, Identifiable{
+    let id: String
     let nome: String
     let donoID: String
     let created_at: Date
 }
 
-struct grupo: Codable, Hashable{
+struct grupo: Codable, Hashable, Identifiable{
+    let id: String
     let nome: String
-    let integrantes: [usuario]
-    let despesas: [despesa]
+    let integrantes: [String]
+    let despesas: [String]
     let created_at: Date
 }   
 
