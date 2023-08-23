437dbaf0c50ca6fa9fbc5a7dfc8642ec2a4edcc1
João Pedro Cruz Espíndola <joaopedro.espindola@outlook.com>
2023-08-23 17:26:20 -0300

    finishing newGroupPage

--- DiviDApp/DiviDAppApp.swift
+++ DiviDApp/DiviDAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DiviDAppApp: App {
     var body: some Scene {
         WindowGroup {
-            NovoGrupo()
+            NewGroupPage(nomeNovosParticipante: [])
         }
     }
 }
